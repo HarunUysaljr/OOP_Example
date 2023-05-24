29731fe0ad7e9092219027301cad5f20d062db7c
Harun <harunuysaljr@gmail.com>
2023-05-25 00:52:30 +0300

    main

---
+++ Encapsulation (Kapsülleme)/EncapsulationExample03/EncapsulationExample03/Models/BankaHesabi.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EncapsulationExample03.Models
+{
+    internal class BankaHesabi
+    {
+        private string hesapNo;
+        private decimal bakiye;
+        public string HesapNo {
+            get { return hesapNo; }
+            private set { hesapNo = value; } 
+        }
+
+        public decimal HesapBakiyesi { 
+            get { return bakiye; }
+            set { bakiye = value; } }
+
+
+        public  BankaHesabi(string hesapNo, decimal baslangicBakiyesi)
+        {
+            HesapNo = hesapNo;
+            HesapBakiyesi = baslangicBakiyesi;
+        }
+
+        public void ParaYatir(decimal miktar)
+        {
+            if (miktar > 0)
+            {
+                HesapBakiyesi += miktar;
+            }
+        }
+
+        public void ParaCek(decimal miktar)
+        {
+            if (miktar > 0 && miktar <= HesapBakiyesi)
+                HesapBakiyesi -= miktar;
+            else
+                Console.WriteLine("Geçersiz işlem!");
+        }
+
+        public void hesapBilgileriGöster()
+        {
+            Console.WriteLine(HesapNo);
+            Console.WriteLine(HesapBakiyesi);
+        }
+    }
+}
+
---
+++ Encapsulation (Kapsülleme)/EncapsulationExample03/EncapsulationExample03/Program.cs
@@ -0,0 +1,23 @@
+﻿using EncapsulationExample03.Models;
+
+BankaHesabi banka = new BankaHesabi("4551", 1524 );
+
+banka.hesapBilgileriGöster();
+
+banka.ParaYatir(2);
+banka.ParaCek(1525);
+banka.hesapBilgileriGöster();
+
+/*
+ İlk olarak, EncapsulationExample03.Models adında bir namespace oluşturulur.
+Ardından BankaHesabi adında bir sınıf tanımlanır. Bu sınıf, bir banka hesabını temsil eder.
+Sınıfın içinde, hesapNo ve bakiye adında özel (private) alanlar bulunur. 
+Bu alanlar sadece sınıf içinde erişilebilir durumdadır.
+HesapNo özelliği, sadece okunabilir bir şekilde tanımlanır. Dışarıdan erişim sağlanabilmesi için get erişimci kullanılır ve set erişimciye private olarak belirlenir.
+HesapBakiyesi özelliği, hem okunabilir hem de yazılabilir bir şekilde tanımlanır. Hem get hem de set erişimcileri kullanılır.
+BankaHesabi sınıfının bir kurucu metodu tanımlanır. Bu metot, bir hesap numarası ve başlangıç bakiyesi alır ve ilgili alanlara değerler atanır.
+ParaYatir metodu, belirli bir miktar para yatırma işlemini gerçekleştirir. Miktarın 0'dan büyük olması durumunda, hesap bakiyesi bu miktar kadar artırılır.
+ParaCek metodu, belirli bir miktar para çekme işlemini gerçekleştirir. Miktarın 0'dan büyük ve hesap bakiyesinden küçük veya eşit olması durumunda, hesap bakiyesi bu miktar kadar azaltılır. Aksi durumda "Geçersiz işlem!" mesajı ekrana yazdırılır.
+HesapBilgileriniGoster metodu, hesap numarasını ve hesap bakiyesini ekrana yazdırır.
+
+ */
