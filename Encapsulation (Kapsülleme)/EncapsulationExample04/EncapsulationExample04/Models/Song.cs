2666211d3ef01ef5a06ca6b086af8eefaf66bf23
Harun <harunuysaljr@gmail.com>
2023-05-27 14:53:48 +0300

    main

---
+++ Encapsulation (Kapsülleme)/EncapsulationExample04/EncapsulationExample04/Models/Song.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EncapsulationExample04.Models
+{
+    internal class Song
+    {
+        private string _title;
+        private bool _playing;
+        private string _artist;
+        private TimeSpan _duraiton;
+
+        public string Title {
+            get
+            {
+                return _title;
+            }
+            set {
+                _title = value;
+            }
+        }
+
+        public string Artist {
+            get
+            {
+                return _artist;
+            }
+            set {
+                _artist = value;
+            }
+        }
+
+        public TimeSpan Duration {
+            get { return _duraiton;  }
+            set { _duraiton = value; } }
+
+        public Boolean Playing {
+            get { return _playing;  }
+            set { _playing = value;  } }
+
+
+
+
+        public void Oynat()
+        {
+            if (!_playing)
+            {
+                Console.WriteLine($"Çalan Şarkı : {Title} - {Artist}");
+                Playing = true;
+            }
+
+        }
+
+        public void Duraklat()
+        {
+            if (Playing)
+            {
+                Console.WriteLine($"Paused: {Title} - {Artist}");
+                Playing= false;
+            }
+            else
+            {
+                Console.WriteLine("Şarkı zaten devam etmiyor");
+            }
+        }
+
+        public void DevamEt()
+        {
+            Console.WriteLine("Devam ET.");
+        }
+    }
+}
--- Encapsulation (Kapsülleme)/EncapsulationExample04/EncapsulationExample04/Program.cs
+++ Encapsulation (Kapsülleme)/EncapsulationExample04/EncapsulationExample04/Program.cs
@@ -1,2 +1,24 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using EncapsulationExample04.Models;
+
+Song song = new Song();
+
+
+song.Title = "Gülpembe";
+song.Artist = "Barış Manço";
+song.Duration = TimeSpan.FromSeconds(54);
+
+Console.WriteLine($"Şarkı: {song.Title} - {song.Artist}");
+Console.WriteLine($"Süresi: {song.Duration}");
+
+song.Oynat();
+
+Console.WriteLine("Durdurmak için bir tuşa basınız....");
+Console.ReadKey();
+
+Console.WriteLine("");
+
+song.Duraklat();
+
+
+Console.ReadLine();
+
