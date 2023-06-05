190626c0f6791c00192a682b3d5f5c9d46a98284
Harun <harunuysaljr@gmail.com>
2023-06-05 16:41:03 +0300

    main

---
+++ İnheritance (Miras Alma)/İnheritanceExample01/İnheritanceExample01/Models/Hayvan.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace İnheritanceExample01.Models
+{
+    public class Hayvan
+    {
+        public string hayvan_isim;
+        public string hayvan_cinsiyet;
+        public string hayvan_rengi;
+        public int hayvan_yas;
+        public void hayvanBilgisiGoster()
+        {
+            Console.WriteLine(hayvan_isim);
+            Console.WriteLine(hayvan_cinsiyet);
+            Console.WriteLine(hayvan_rengi);
+            Console.WriteLine(hayvan_yas);
+        }
+    }
+}
---
+++ İnheritance (Miras Alma)/İnheritanceExample01/İnheritanceExample01/Program.cs
@@ -0,0 +1,28 @@
+﻿using İnheritanceExample01.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace İnheritanceExample01
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            kedi kedi = new kedi();
+
+            kedi.hayvan_isim = "Ares";
+            kedi.hayvan_yas = 1;
+            kedi.hayvan_rengi = "Sarı";
+            kedi.hayvan_cinsiyet = "Erkek";
+            kedi.kediPatisi = "2";
+            kedi.hayvanBilgisiGoster();
+
+            Console.ReadLine();
+
+        }
+    }
+}
