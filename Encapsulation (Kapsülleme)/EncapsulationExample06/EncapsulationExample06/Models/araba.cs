1d9089a8c2e888285eb7968527c3e2990ec3939b
Harun <harunuysaljr@gmail.com>
2023-08-08 14:10:12 +0300

    main

---
+++ Encapsulation (Kapsülleme)/EncapsulationExample06/EncapsulationExample06/Models/araba.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EncapsulationExample06.Models
+{
+    internal class araba
+    {
+        public int hız;
+        public int h;
+        public string a;
+        public string b; public string c; public string d;
+    }
+}
---
+++ Encapsulation (Kapsülleme)/EncapsulationExample06/EncapsulationExample06/Program.cs
@@ -0,0 +1,22 @@
+﻿using EncapsulationExample06.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EncapsulationExample06
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            araba car = new araba();
+
+            car.hız = 10;
+        
+
+               
+        }
+    }
+}
